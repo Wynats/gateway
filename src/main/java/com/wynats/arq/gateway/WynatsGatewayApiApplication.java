fa1de9b355c3008b60fc009ba562663693503f2f
Beatriz Lopez <beatlm@gmail.com>
2018-10-06 22:49:50 +0200

    Refactor and logger filter

--- src/main/java/com/wynats/arq/gateway/WynatsGatewayApiApplication.java
+++ src/main/java/com/wynats/arq/gateway/WynatsGatewayApiApplication.java
@@ -1,9 +1,12 @@
-package com.wynats.arq.wynatsgatewayapi;
+package com.wynats.arq.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.wynats.arq.gateway.filter.LoggerFilter;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -13,4 +16,8 @@ public class WynatsGatewayApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WynatsGatewayApiApplication.class, args);
 	}
+	@Bean
+	public LoggerFilter preFilter() {
+        return new LoggerFilter();
+    }
 }
